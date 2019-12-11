4cb64f5bf77d810c869e876e7b9d7b8d17229738
jaksteurer <jaksteurer@tsn.at>
2019-12-11 08:06:15 +0100

    initial

--- src/String.java
+++ src/String.java
@@ -1,22 +1,18 @@
-import java.util.Arrays;
-
 public class String {
 
-	public static void main(String[] args) {
+	public static void main(java.lang.String[] args) {
 		compareTo();
 	}
 	
 	public static void compareTo() {
 		 java.lang.String name2 = "Mayer";
 		 java.lang.String name1 = "Meier";
-		 name1.toLowerCase();
-		 name2.toLowerCase();
 		 if(name1.compareTo(name2)>0)
 		 {
-			 System.out.println(name1 + " "+ name2);
+			 System.out.println(name1 + "\n"+ name2);
 		 }
 		 else {
-			 System.out.println(name2 + " "+ name1);
+			 System.out.println(name2 + "\n"+ name1);
 		 }
 	}
 }
